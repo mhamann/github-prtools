654ee30b6e92a6f5e204045ec6f13ee8f5c04e00
Matt Hamann <mhamann@us.ibm.com>
2017-06-18 23:53:02 -0400

    Add github defaults

--- utils/conf.js
+++ utils/conf.js
@@ -3,5 +3,6 @@ const nconf = require('nconf');
 nconf.argv()
      .env({ lowerCase: true, separator: '__' })
      .defaults({
-       logger: { level: 'info' }
+       logger: { level: 'info' },
+       github: { token: null }
      });
