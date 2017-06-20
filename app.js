463f0dba941ba8939be0f83c3aac6b457ef67b0f
Matt Hamann <mhamann@us.ibm.com>
2017-06-19 22:26:44 -0400

    Fix issues with GH secret handling; Provide better error responses.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const nconf = require('nconf');
 const logger = require('./utils/logging').createLogger('main');
 const routes = require('./routes');
 const bodyParser = require('body-parser');
+const errorHandler = require('strong-error-handler');
 
 // Some setup
 require('./lib/pulls');
@@ -14,6 +15,11 @@ app.set('port', nconf.get('port') || 3830);
 
 app.use(bodyParser.json());
 app.use(routes);
+
+app.use(errorHandler({
+  debug: app.get('env') === 'development',
+  log: true,
+}));
 
 app.listen(app.get('port'), function() {
   logger.info(`Server listening on ${app.get('host')}:${app.get('port')}`);
--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,10 @@ const crypto = require('crypto');
 // Validate HMAC signatures if configured
 if (nconf.get('auth:secret') !== null) {
   router.use('/v1/events', function(req, res, next) {
-    let signature = req.get('X-Hub-Signature');
-    let hash = crypto.createHmac('sha1', nconf.get('auth:secret')).update(JSON.stringify(req.body)).digest('hex');
+    let signature = req.get('X-Hub-Signature').split('=');
+    let hash = crypto.createHmac(signature[0], nconf.get('auth:secret')).update(JSON.stringify(req.body)).digest('hex');
     console.log(hash);
-    if (hash !== signature) {
+    if (hash !== signature[1]) {
       let err = new Error('Signature mismatch');
       err.statusCode = 400;
       throw err;
